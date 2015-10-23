f1424102e75c3c43b7cf8b4084ebb13776d6a817
Ed Glazenski <edglazenski@gmail.com>
2015-10-22 20:14:52 -0400

    Initial commit. JSON requests are currently working. Currently working on styling.

---
+++ js/app.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+    $('form').submit(function(event){
+      event.preventDefault();
+      
+      $('#image-funfacts').children().remove();
+      $('#artist-name').children().remove();
+      var band = $('#search').val();
+
+
+      //URL build
+      var bucketList = ['hotttnesss', 'familiarity', 'artist_location', 'images', 'biographies', 'news', 'songs', 'video'];
+      var key = "api_key=TYBT0MP1Z3LXJEDIM&";
+      var searchURL = "http://developer.echonest.com/api/v4/artist/search?" + key;
+
+          //Adds bucket items from bucketList array to url
+       
+          for(i = 0; i < bucketList.length; i++){
+            searchURL += 'bucket=' + bucketList[i] + "&";
+            console.log(searchURL);
+           }
+
+      //end URL build
+      
+      var bandsOptions = { 
+        name: band,
+        start: 0,
+        results: 1
+       
+      };
+      
+      function displayArtists(response) {
+
+        console.log(response);
+          
+        $.each(response.response.artists, function(i, item){
+            $('#artist-name').append('<h2>' + item.name + '</h2>');
+            $('#image-funfacts').append('<img src="' + item.images[0].url + '"/>');
+            $('#image-funfacts').append('<h3>Hometown:</h3><p>' + item.artist_location.location + '</p>');
+            $('#image-funfacts').append('<h3>World Familiarity:</h3><p>' + Math.floor(item.familiarity * 100)) + '</p>';
+            musicianHTML += '<li><p>' + Math.floor(item.hotttnesss * 100) + '%</p></li>';
+            musicianHTML += '<li><p>' + item.biographies[0].text + '</p></li>';
+            musicianHTML += '<li><p>' + item.news[0].name + '</p></li>';
+            musicianHTML += '<li><p>' + item.news[0].summary + '</p></li>';
+            musicianHTML += '<li><a href="' + item.news[0].url + '">' + item.news[0].name + '</a></li>';
+            musicianHTML += '<li><video src="' + item.video[0].url + '"></video></li>';
+        });
+        
+        
+        $('#results').html(musicianHTML); 
+      }
+      $.getJSON(searchURL, bandsOptions, displayArtists);
+    });
